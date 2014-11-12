6aa2dfd1a5bb53da6ff5853a38c93ded0ade0c5b
patrickfrey <patrickpfrey@yahoo.com>
2014-11-12 13:15:25 +0100

    no nested classes anymore

--- include/strus/strus.hpp
+++ include/strus/strus.hpp
@@ -32,6 +32,7 @@
 #include "strus/postingIteratorInterface.hpp"
 #include "strus/queryProcessorInterface.hpp"
 #include "strus/storageInterface.hpp"
+#include "strus/storageInserterInterface.hpp"
 #include "strus/queryEvalLib.hpp"
 #include "strus/queryProcessorLib.hpp"
 #include "strus/storageLib.hpp"
