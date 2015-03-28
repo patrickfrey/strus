e250c6aa0d7ce60989f22b553e791ffa91aa326b
Andreas Baumann <mail@andreasbaumann.cc>
2015-03-28 13:55:07 +0100

    first prelimiary Debian files for OBS

--- dist/obs/deploy_to_obs.sh
+++ dist/obs/deploy_to_obs.sh
@@ -8,18 +8,33 @@ rm -f ${PACKAGE_NAME}-${PACKAGE_VERSION}.tar.gz
 cmake .
 make dist-gz
 
-# Redhat
+# Redhat/SuSE
 
 cp ${PACKAGE_NAME}-${PACKAGE_VERSION}.tar.gz $OSC_HOME/${PACKAGE_NAME}_${PACKAGE_VERSION}.tar.gz
 cp dist/redhat/$PACKAGE_NAME.spec $OSC_HOME/$PACKAGE_NAME.spec
 
 # Debian/Ubuntu
 
-SIZE=`stat -c '%s' $OSC_HOME/${PACKAGE_NAME}_${PACKAGE_VERSION}.tar.gz`
-CHKSUM=`md5sum $OSC_HOME/${PACKAGE_NAME}_${PACKAGE_VERSION}.tar.gz  | cut -f 1 -d' '`
+cp ${PACKAGE_NAME}-${PACKAGE_VERSION}.tar.gz $OSC_HOME/${PACKAGE_NAME}_${PACKAGE_VERSION}.orig.tar.gz
 
-cat dist/obs/$PACKAGE_NAME.dsc > $OSC_HOME/$PACKAGE_NAME.dsc
-echo " $CHKSUM $SIZE ${PACKAGE_NAME}_${PACKAGE_VERSION}.tar.gz" >> $OSC_HOME/$PACKAGE_NAME.dsc
+SIZE=`stat -c '%s' $OSC_HOME/${PACKAGE_NAME}_${PACKAGE_VERSION}.orig.tar.gz`
+CHKSUM=`md5sum $OSC_HOME/${PACKAGE_NAME}_${PACKAGE_VERSION}.orig.tar.gz | cut -f 1 -d' '`
+
+cp dist/obs/$PACKAGE_NAME.dsc $OSC_HOME/$PACKAGE_NAME.dsc
+echo " $CHKSUM $SIZE ${PACKAGE_NAME}_${PACKAGE_VERSION}.orig.tar.gz" >> $OSC_HOME/$PACKAGE_NAME.dsc
+
+TMPDIR=/tmp
+rm -f $OSC_HOME/${PACKAGE_NAME}_${PACKAGE_VERSION}.debian.tar.gz
+rm -rf $TMPDIR/debian
+cp -r dist/debian $TMPDIR/.
+OLDDIR=$PWD
+cd $TMPDIR
+tar zcf $TMPDIR/${PACKAGE_NAME}_${PACKAGE_VERSION}.debian.tar.gz debian
+cd $OLDDIR
+mv -f $TMPDIR/${PACKAGE_NAME}_${PACKAGE_VERSION}.debian.tar.gz $OSC_HOME/.
+DEBIAN_SIZE=`stat -c '%s' $OSC_HOME/${PACKAGE_NAME}_${PACKAGE_VERSION}.debian.tar.gz`
+DEBIAN_CHKSUM=`md5sum  $OSC_HOME/${PACKAGE_NAME}_${PACKAGE_VERSION}.debian.tar.gz | cut -f 1 -d' '`
+echo " $DEBIAN_CHKSUM $DEBIAN_SIZE ${PACKAGE_NAME}_${PACKAGE_VERSION}.debian.tar.gz" >> $OSC_HOME/$PACKAGE_NAME.dsc
 
 # Archlinux
 
