0302c2544098446e605e23d3f61ca303b74c1f5f
patrickfrey <patrickpfrey@yahoo.com>
2015-05-07 12:25:29 +0200

    ...

--- src/storage/storageClient.cpp
+++ src/storage/storageClient.cpp
@@ -58,7 +58,6 @@
 #include <vector>
 #include <cstring>
 #include <cstdio>
-/*[-]*/#include <iostream>
 
 using namespace strus;
 
@@ -603,7 +602,6 @@ Index StorageClient::documentStatistics(
 				for (;ei != ee; ei = invblk.next( ei))
 				{
 					InvTerm it = invblk.element_at( ei);
-					/*[-]*/std::cout << it.typeno << ' ' << it.termno << " " << it.ff << std::endl;
 					if (typeno == it.typeno)
 					{
 						rt += 1;
