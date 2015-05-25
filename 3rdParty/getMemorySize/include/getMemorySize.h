6ff74de071654197bf84e3adf12e7aa9988364db
patrickfrey <patrickpfrey@yahoo.com>
2015-05-25 13:54:25 +0200

    added get memory size function

---
+++ 3rdParty/getMemorySize/include/getMemorySize.h
@@ -0,0 +1,18 @@
+/*
+ *  Header file for linking getMemorySize as static library
+*/ 
+#ifndef STRUS_3RDPARTY_GET_MEMORY_SIZE_INCLUDED
+#define STRUS_3RDPARTY_GET_MEMORY_SIZE_INCLUDED
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+size_t getMemorySize();
+
+#ifdef __cplusplus
+extern "C"
+}
+#endif
+
