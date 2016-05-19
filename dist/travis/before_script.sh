f8af7fe58930eacdd8d00e84d7d9ca4434faabfd
Andreas Baumann <mail@andreasbaumann.cc>
2016-05-19 16:18:04 +0200

    first trial to build on Travis

---
+++ dist/travis/before_script.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+OS=$(uname -s)
+
+case $OS in
+	Linux)
+		sudo apt-get update -qq
+		sudo apt-get install -y \
+			cmake \
+			libboost-all-dev \
+			libleveldb-dev
+		;;
+		
+	Darwin)
+		brew update
+		if test "X$CC" = "Xgcc"; then
+			brew install gcc48 --enable-all-languages || true
+			brew link --force gcc48
+		fi
+		brew install \
+			cmake \
+			boost \
+			gettext \
+			leveldb \
+			|| true
+		# make sure cmake finds the brew version of gettext
+		brew link --force gettext
+		;;
+	
+	default)
+		echo "ERROR: unknown operating system '$OS'."
+		;;
+esac
+
---
+++ dist/travis/script.sh
@@ -0,0 +1,106 @@
+#!/bin/sh
+
+OS=$(uname -s)
+
+# set up environment
+case $OS in
+	Linux)
+		;;
+	
+	Darwin)
+		if test "X$CC" = "Xgcc"; then
+			# gcc on OSX is a mere frontend to clang, force using gcc 4.8
+			export CXX=g++-4.8
+			export CC=gcc-4.8
+		fi
+		# forcing brew versions (of gettext) over Mac versions
+		export CFLAGS=-I/usr/local
+		export CXXFLAGS=-I/usr/local
+		export LDFLAGS=-L/usr/local/lib
+		;;
+
+	*)
+		echo "ERROR: unknown operating system '$OS'."
+		;;
+esac
+
+DEPS="strusBase"
+
+# build pre-requisites
+for i in $DEPS; do
+	git clone `git config remote.origin.url | sed "s@/strus\.@/$i.@g"` $i
+	cd $i
+	git checkout travis
+	case $OS in
+		Linux)
+			cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release \
+				-DLIB_INSTALL_DIR=lib -DCMAKE_CXX_FLAGS=-g \
+				.
+			make
+			make test
+			sudo make install
+			;;
+		
+		Darwin)
+			if test "X$CC" = "Xgcc"; then
+				cmake \
+					-DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release \
+					-DCMAKE_CXX_FLAGS=-g -G 'Unix Makefiles' \
+					.
+				make
+				make test
+				sudo make install
+			else
+				cmake \
+					-DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release \
+					-DCMAKE_CXX_FLAGS=-g -G Xcode \
+					.
+				xcodebuild -configuration Release -target ALL_BUILD
+				xcodebuild -configuration Release -target RUN_TESTS
+				sudo xcodebuild -configuration Release -target install
+			fi
+			;;
+
+		*)
+			echo "ERROR: unknown operating system '$OS'."
+			;;
+	esac
+	cd ..
+done
+
+# build the package itself
+case $OS in
+	Linux)
+		cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release \
+			-DLIB_INSTALL_DIR=lib -DCMAKE_CXX_FLAGS=-g \
+			.
+		make
+		make test
+		sudo make install
+		;;
+
+	Darwin)
+		if test "X$CC" = "Xgcc"; then
+			cmake \
+				-DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release \
+				-DCMAKE_CXX_FLAGS=-g -G 'Unix Makefiles' \
+				.
+			make
+			make test
+			sudo make install
+		else
+			cmake \
+				-DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release \
+				-DCMAKE_CXX_FLAGS=-g -G Xcode \
+				.
+			xcodebuild -configuration Release -target ALL_BUILD
+			xcodebuild -configuration Release -target RUN_TESTS
+			sudo xcodebuild -configuration Release -target install
+		fi
+		;;
+		
+	*)
+		echo "ERROR: unknown operating system '$OS'."
+		;;
+esac
+	
