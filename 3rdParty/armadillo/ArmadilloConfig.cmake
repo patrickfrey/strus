ed85bccb1bc36fadb4182612686441dd7842b636
patrickfrey <patrickpfrey@yahoo.com>
2016-08-27 18:01:36 +0200

    ...

--- 3rdParty/armadillo/ArmadilloConfig.cmake
+++ 3rdParty/armadillo/ArmadilloConfig.cmake
@@ -5,11 +5,11 @@
 #  ARMADILLO_LIBRARIES    - libraries to link against
 
 # Tell the user project where to find our headers and libraries
-set(ARMADILLO_INCLUDE_DIRS "/home/patrick/Projects/armadillo/armadillo-7.400.2;/home/patrick/Projects/armadillo/armadillo-7.400.2")
-set(ARMADILLO_LIBRARY_DIRS "/home/patrick/Projects/armadillo/armadillo-7.400.2")
+set(ARMADILLO_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}/3rdParty/armadillo")
+set(ARMADILLO_LIBRARY_DIRS "${PROJECT_SOURCE_DIR}/3rdParty/armadillo")
 
 # Our library dependencies (contains definitions for IMPORTED targets)
-include("/home/patrick/Projects/armadillo/armadillo-7.400.2/ArmadilloLibraryDepends.cmake")
+include("${PROJECT_SOURCE_DIR}/3rdParty/armadillo/ArmadilloLibraryDepends.cmake")
 
 # These are IMPORTED targets created by ArmadilloLibraryDepends.cmake
 set(ARMADILLO_LIBRARIES armadillo)
