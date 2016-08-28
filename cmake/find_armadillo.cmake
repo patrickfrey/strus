72fae51fde603d3a5a87b07e0ca33df883301306
patrickfrey <patrickpfrey@yahoo.com>
2016-08-28 21:20:21 +0200

    try to make it run with ubuntu default armadillo library -- not succeeded yet

--- cmake/find_armadillo.cmake
+++ cmake/find_armadillo.cmake
@@ -359,7 +359,7 @@ if (ARMADILLO_FOUND)
   endif ()
   message(STATUS "Armadillo libraries: ${ARMADILLO_LIBRARIES}")
 endif ()
-set( ARMADILLO_LIBRARIES ${ARMADILLO_LIBRARIES} lapackpp )
+set( ARMADILLO_LIBRARIES ${ARMADILLO_LIBRARIES} )
 
 # Hide internal variables
 mark_as_advanced(
