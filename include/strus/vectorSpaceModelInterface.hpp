6f3d94108745f0a56d1e8b8c577a593ddfcc3b80
patrickfrey <patrickpfrey@yahoo.com>
2016-10-25 19:51:22 +0200

    some more methods in vector space model interface

--- include/strus/vectorSpaceModelInterface.hpp
+++ include/strus/vectorSpaceModelInterface.hpp
@@ -28,10 +28,28 @@ public:
 	/// \brief Destructor
 	virtual ~VectorSpaceModelInterface(){}
 
+	/// \brief Create a data repository for the data of a vector space model
+	/// \param[in] configsource configuration string of the model and the database (not a filename!)
+	/// \param[in] database database type for the repository
+	/// \return true on success, false if an error occurred
+	/// \remark This method has to be called before any other operation on a vector space model
+	virtual bool createRepository(
+			const std::string& configsource,
+			const DatabaseInterface* database)=0;
+
+	/// \brief Reset an existing repository for the data of a vector space model to its initial state after creation
+	/// \param[in] configsource configuration string of the model and the database (not a filename!)
+	/// \param[in] database database type for the repository
+	/// \return true on success, false if an error occurred
+	virtual bool resetRepository(
+			const std::string& configsrc,
+			const DatabaseInterface* database)=0;
+
 	/// \brief Create a new vector space model instance
 	/// \param[in] configsource configuration string of the model and the database (not a filename!)
 	/// \param[in] database database type of the persistent storage where to load the model data from
 	/// \return the instance (with ownership)
+	/// \remark The repository refered to by the configuration must exist and must have been built (-> createRepository)
 	virtual VectorSpaceModelInstanceInterface* createInstance(
 			const std::string& configsource,
 			const DatabaseInterface* database) const=0;
@@ -40,6 +58,7 @@ public:
 	/// \param[in] configsource configuration string of the model and the database (not a filename!)
 	/// \param[in] database database type to use as persistent storage
 	/// \return the builder (with ownership)
+	/// \remark The repository refered to by the configuration must exist (-> createRepository)
 	virtual VectorSpaceModelBuilderInterface* createBuilder(
 			const std::string& configsource,
 			const DatabaseInterface* database) const=0;
