c791bfbe9bfadda01d5cad932b5203f438182660
patrickfrey <patrickpfrey@yahoo.com>
2017-01-02 15:06:50 +0100

    ...

--- tests/loggingMalloc/src/testLoggingMalloc.cpp
+++ tests/loggingMalloc/src/testLoggingMalloc.cpp
@@ -5,7 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-#define STRUS_TEST_LOGGING_MALLOC
+#define _STRUS_TEST_LOGGING_MALLOC
 #include "loggingMalloc.c"
 #include <stdexcept>
 
