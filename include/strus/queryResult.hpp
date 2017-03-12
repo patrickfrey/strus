aa1c43e658c391d889e2813599f5e74727694354
patrickfrey <patrickpfrey@yahoo.com>
2017-03-12 14:02:24 +0100

    shorter method names in QueryResult

--- include/strus/queryResult.hpp
+++ include/strus/queryResult.hpp
@@ -27,34 +27,34 @@ public:
 	/// \brief Copy constructor
 	QueryResult( const QueryResult& o)
 		:m_evaluationPass(o.m_evaluationPass)
-		,m_nofDocumentsRanked(o.m_nofDocumentsRanked)
-		,m_nofDocumentsVisited(o.m_nofDocumentsVisited)
+		,m_nofRanked(o.m_nofRanked)
+		,m_nofVisited(o.m_nofVisited)
 		,m_ranks(o.m_ranks){}
 	/// \brief Constructor
 	QueryResult(
 			unsigned int evaluationPass_,
-			unsigned int nofDocumentsRanked_,
-			unsigned int nofDocumentsVisited_,
+			unsigned int nofRanked_,
+			unsigned int nofVisited_,
 			const std::vector<ResultDocument>& ranks_)
 		:m_evaluationPass(evaluationPass_)
-		,m_nofDocumentsRanked(nofDocumentsRanked_)
-		,m_nofDocumentsVisited(nofDocumentsVisited_)
+		,m_nofRanked(nofRanked_)
+		,m_nofVisited(nofVisited_)
 		,m_ranks(ranks_){}
 
 	/// \brief Get the last query evaluation pass used (level of selection features used)
 	unsigned int evaluationPass() const				{return m_evaluationPass;}
 	/// \brief Get the total number of matches that were ranked (after applying all query restrictions)
-	unsigned int nofDocumentsRanked() const				{return m_nofDocumentsRanked;}
+	unsigned int nofRanked() const					{return m_nofRanked;}
 	/// \brief Get the total number of matches that were visited (after applying ACL restrictions, but before applying other restrictions)
-	unsigned int nofDocumentsVisited() const			{return m_nofDocumentsVisited;}
+	unsigned int nofVisited() const					{return m_nofVisited;}
 
 	/// \brief Get the list of result elements
 	const std::vector<ResultDocument>& ranks() const		{return m_ranks;}
 
 private:
 	unsigned int m_evaluationPass;			///< query evaluation passes used (level of selection features used)
-	unsigned int m_nofDocumentsRanked;		///< total number of matches for a query with applying restrictions (might be an estimate)
-	unsigned int m_nofDocumentsVisited;		///< total number of matches for a query without applying restrictions but ACL restrictions (might be an estimate)
+	unsigned int m_nofRanked;			///< total number of matches for a query with applying restrictions (might be an estimate)
+	unsigned int m_nofVisited;			///< total number of matches for a query without applying restrictions but ACL restrictions (might be an estimate)
 	std::vector<ResultDocument> m_ranks;		///< list of result documents (part of the total result)
 };
 
