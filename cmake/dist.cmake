f21eb2fcb44f60ed96968706bead1cfe1d69c697
Andreas Baumann <mail@andreasbaumann.cc>
2017-12-14 20:14:05 +0100

    removed dist-bz2 und dist-xz targets

--- cmake/dist.cmake
+++ cmake/dist.cmake
@@ -5,25 +5,13 @@ string(TOLOWER ${CMAKE_PROJECT_NAME} PACKAGE_NAME)
 set(ARCHIVE_NAME ${PACKAGE_NAME}-${STRUS_VERSION})
 IF (NOT TARGET dist)
 add_custom_target(dist
-    COMMAND git archive --prefix=${ARCHIVE_NAME}/ HEAD
-        > ${CMAKE_BINARY_DIR}/${ARCHIVE_NAME}.tar
+    COMMAND ${CMAKE_SOURCE_DIR}/dist/helpers/git-archive-all.sh --prefix ${ARCHIVE_NAME}/ -t HEAD
+        --format tar ${CMAKE_BINARY_DIR}/${ARCHIVE_NAME}.tar
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
 ENDIF (NOT TARGET dist )
-IF (NOT TARGET dist-bz2 )
-add_custom_target(dist-bz2
-    COMMAND git archive --prefix=${ARCHIVE_NAME}/ HEAD
-        | bzip2 > ${CMAKE_BINARY_DIR}/${ARCHIVE_NAME}.tar.bz2
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
-ENDIF (NOT TARGET dist-bz2 )
 IF (NOT TARGET dist-gz )
 add_custom_target(dist-gz
-    COMMAND git archive --prefix=${ARCHIVE_NAME}/ HEAD
-        | gzip > ${CMAKE_BINARY_DIR}/${ARCHIVE_NAME}.tar.gz
+    COMMAND ${CMAKE_SOURCE_DIR}/dist/helpers/git-archive-all.sh --prefix ${ARCHIVE_NAME}/ -t HEAD
+        --format tar.gz ${CMAKE_BINARY_DIR}/${ARCHIVE_NAME}.tar.gz
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
 ENDIF (NOT TARGET dist-gz )
-IF (NOT TARGET dist-xz )
-add_custom_target(dist-xz
-    COMMAND git archive --prefix=${ARCHIVE_NAME}/ HEAD
-        | xz > ${CMAKE_BINARY_DIR}/${ARCHIVE_NAME}.tar.xz
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
-ENDIF (NOT TARGET dist-xz )
