7bb9dcee5e591754c27aabf9ba15d59aea7669a4
patrickfrey <patrickpfrey@yahoo.com>
2017-12-21 22:11:09 +0100

    updated travis before installation script to be similar to strusBindings that fails to get a hint for the failing build of the later

--- dist/travis/before_script.sh
+++ dist/travis/before_script.sh
@@ -15,30 +15,15 @@ case $OS in
 		
 	Darwin)
 		brew update
-		if test "X$CC" = "Xgcc"; then
-			brew install gcc48 --enable-all-languages || true
-			brew link --force gcc48 || true
-		fi
-		brew install \
-			cmake \
-			boost \
-			gettext \
-			snappy \
-			leveldb \
-			|| true
+		brew upgrade cmake
+		brew upgrade boost
+		brew install gettext snappy leveldb || true
 		# make sure cmake finds the brew version of gettext
 		brew link --force gettext || true
 		brew link leveldb || true
 		brew link snappy || true
-		# rebuild leveldb to use gcc-4.8 ABI on OSX (libstc++ differs
-		# from stdc++, leveldb uses std::string in API functions, C
-		# libraries like gettext and snappy and even boost do not 
-		# have this problem)
-		if test "X$CC" = "Xgcc"; then
-			brew reinstall leveldb --cc=gcc-4.8
-		fi
 		;;
-	
+
 	*)
 		echo "ERROR: unknown operating system '$OS'."
 		;;
