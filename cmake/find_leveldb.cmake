af4c48bf76e302bc5f2a4b3e89d06acb9ab6e102
patrickfrey <patrickpfrey@yahoo.com>
2017-12-23 11:31:27 +0100

    cmake: try to fix relocation of leveldb on travis build that does not work always

--- cmake/find_leveldb.cmake
+++ cmake/find_leveldb.cmake
@@ -1,18 +1,60 @@
-# from https://github.com/justmoon/bzing/blob/master/cmake/FindLevelDB.cmake, slightly adapted
+# original from https://github.com/justmoon/bzing/blob/master/cmake/FindLevelDB.cmake, adapted
 
-find_path( LevelDB_INCLUDE_PATH NAMES leveldb/db.h HINTS "${LEVELDB_ROOT}/include"  "${CMAKE_INSTALL_PREFIX}/include/strus"  "${CMAKE_INSTALL_PREFIX}/include" )
-find_library( LevelDB_LIBRARY NAMES leveldb HINTS "${LEVELDB_ROOT}/lib" "${CMAKE_INSTALL_PREFIX}/${LIB_INSTALL_DIR}" "${CMAKE_INSTALL_PREFIX}/${LIB_INSTALL_DIR}/strus" )
+if( NOT LEVELDB_ROOT)
+if( APPLE)
+execute_process( COMMAND  brew  --prefix  leveldb
+			   RESULT_VARIABLE  RET_LEVELDB_PATH
+			   OUTPUT_VARIABLE  LEVELDB_INSTALL_PATH
+			   OUTPUT_STRIP_TRAILING_WHITESPACE )
+if( ${RET_LEVELDB_PATH} STREQUAL "" OR ${RET_LEVELDB_PATH} STREQUAL "0" )
+set( LEVELDB_ROOT LEVELDB_INSTALL_PATH )
+endif( ${RET_LEVELDB_PATH} STREQUAL "" OR ${RET_LEVELDB_PATH} STREQUAL "0" )
+endif( APPLE )
+endif (NOT LEVELDB_ROOT)
+if( LEVELDB_ROOT )
+MESSAGE( STATUS "Installation path of leveldb: '${LEVELDB_ROOT}' " )
+endif( LEVELDB_ROOT )
+
+find_path( LevelDB_INCLUDE_PATH NAMES  "db.h"
+			HINTS ${LEVELDB_ROOT}/include ${LEVELDB_ROOT}/include/leveldb ${LEVELDB_ROOT}
+			NO_CMAKE_ENVIRONMENT_PATH
+			NO_CMAKE_PATH
+			NO_SYSTEM_ENVIRONMENT_PATH
+			NO_CMAKE_SYSTEM_PATH )
+if( NOT LevelDB_INCLUDE_PATH )
+find_path( LevelDB_INCLUDE_PATH NAMES leveldb/db.h
+			HINTS "${LEVELDB_ROOT}/include"  "${CMAKE_INSTALL_PREFIX}/include/strus"
+				  "${CMAKE_INSTALL_PREFIX}/include" )
+endif( NOT LevelDB_INCLUDE_PATH )
+
+find_library( LevelDB_LIBRARY NAMES leveldb
+			HINTS "${LEVELDB_ROOT}/${LIB_INSTALL_DIR}" "${LEVELDB_ROOT}/lib"
+			NO_DEFAULT_PATH
+			NO_CMAKE_ENVIRONMENT_PATH
+			NO_CMAKE_PATH
+          		NO_SYSTEM_ENVIRONMENT_PATH
+			NO_CMAKE_SYSTEM_PATH )
+if( NOT LevelDB_LIBRARY )
+find_library( LevelDB_LIBRARY NAMES leveldb
+			HINTS "${LEVELDB_ROOT}/lib" "${CMAKE_INSTALL_PREFIX}/${LIB_INSTALL_DIR}"
+					 "${CMAKE_INSTALL_PREFIX}/${LIB_INSTALL_DIR}/strus" )
+endif( NOT LevelDB_LIBRARY )
 
 if(LevelDB_INCLUDE_PATH AND LevelDB_LIBRARY)
   set(LevelDB_FOUND TRUE)
   get_filename_component( LevelDB_LIBRARY_PATH  ${LevelDB_LIBRARY}  PATH )
+  get_filename_component( LevelDB_LIBRARY  ${LevelDB_LIBRARY}  NAME )
+else(LevelDB_INCLUDE_PATH AND LevelDB_LIBRARY)
+  MESSAGE( STATUS "Found include path of leveldb: '${LevelDB_INCLUDE_PATH}' " )
+  MESSAGE( STATUS "Found library of leveldb: '${LevelDB_LIBRARY}' " )
 endif(LevelDB_INCLUDE_PATH AND LevelDB_LIBRARY)
 
 if(LevelDB_FOUND)
   if(NOT LevelDB_FIND_QUIETLY)
-    message(STATUS "Found LevelDB: ${LevelDB_LIBRARY}")
-    message(STATUS "LevelDB include: ${LevelDB_INCLUDE_PATH}")
-    message(STATUS "LevelDB library: ${LevelDB_LIBRARY}")
+    message( STATUS "Found LevelDB")
+    message( STATUS "LevelDB include path: ${LevelDB_INCLUDE_PATH}")
+    message( STATUS "LevelDB library path: ${LevelDB_LIBRARY_PATH}")
+    message( STATUS "LevelDB library: ${LevelDB_LIBRARY}")
   endif(NOT LevelDB_FIND_QUIETLY)
 else(LevelDB_FOUND)
   if(LevelDB_FIND_REQUIRED)
