c862cc0739e4e7f1f9ae789dd74720ecc80dd41d
patrickfrey <patrickpfrey@yahoo.com>
2018-05-23 11:10:16 +0200

    set global hidden visibility flag

--- cmake/build_rules.cmake
+++ cmake/build_rules.cmake
@@ -25,6 +25,7 @@ MESSAGE( STATUS "Debug postfix: '${CMAKE_DEBUG_POSTFIX}'" )
 
 set_property(GLOBAL PROPERTY rule_launch_compile ccache)
 set_property(GLOBAL PROPERTY rule_launch_link ccache)
+set( CMAKE_CXX_VISIBILITY_PRESET hidden )
 
 if("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU")
 set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC -Wall -Wshadow -pedantic -Wfatal-errors -fvisibility=hidden" )
