3dfe16f0a84e5053daa0bb4407e92603691bb71e
PatrickFrey <patrickpfrey@yahoo.com>
2019-01-24 12:08:11 +0100

    updated description for gh-pages (doxygen) update

--- doc/update.sh
+++ doc/update.sh
@@ -1,12 +1,12 @@
 git checkout gh-pages 
 cd doc/
 git pull origin master 
-rm doxygen/html/*
+git rm doxygen/html/*
 doxygen doxygen.conf
 git status
 git add doxygen/html/*
 cd ..
-git commit -m "updated interface documentation (version 0.14)"
+git commit -m "updated interface documentation (version 0.17)"
 git push origin gh-pages 
 git checkout master 
 
