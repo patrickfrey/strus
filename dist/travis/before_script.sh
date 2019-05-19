f6318a18f0eff691fad7184f6692624f10f75737
PatrickFrey <patrickpfrey@yahoo.com>
2019-05-19 18:13:18 +0200

    updated travis build scripts, switch from trusty to xenial

--- dist/travis/before_script.sh
+++ dist/travis/before_script.sh
@@ -9,10 +9,8 @@ case $OS in
 		sudo apt-get update -qq
 		sudo apt-get install -y \
 			cmake \
-			libleveldb-dev
-		sudo add-apt-repository -y ppa:kojoley/boost
-		sudo apt-get -q update
-		sudo apt-get install libboost-atomic1.58-dev libboost-thread1.58-dev libboost-system1.58-dev libboost-filesystem1.58-dev libboost-regex1.58-dev
+			libleveldb-dev \
+			libboost-all-dev
 		;;
 		
 	Darwin)
